40d7b2449f7796a866f83a2fe7510e921b68f846
unixo <unixo@devzero.it>
2012-06-29 19:30:48 +0200

    added some doc-blocks

--- src/asexplorer/command/CommandBase.java
+++ src/asexplorer/command/CommandBase.java
@@ -11,19 +11,51 @@ import javax.naming.InitialContext;
 abstract public class CommandBase
 {
 
+    /**
+     * Returns the list of command-line arguments supported by the module
+     *
+     * @return Array of long parameter(s)
+     */
     public ArrayList<LongOpt> getParameters()
     {
         return null;
     }
 
+    /**
+     * This method is invoked by Config.parseArguments whenever a unrecognized
+     * parameter is found: in this case, all modules are asked to handle the
+     * parameter.
+     *
+     * @param param The parameter name to be analyzed
+     * @param value The parameter value
+     * @return True if parameter was handled by the module
+     * @see Config
+     */
     public boolean parseParameter(String param, String value)
     {
         return false;
     }
 
+    /**
+     * Returns the command name representing this module: user can specify this
+     * command with --command <name> parameter
+     *
+     * @return The command name
+     */
     abstract public String getCommandName();
 
+    /**
+     * Returns a brief description of the module to display when help is requested
+     *
+     * @return A string description
+     */
     abstract public String getDescription();
 
+    /**
+     * Once connection was established with application server, main asks the
+     * module to execute its code
+     *
+     * @param ctx The initial context representing connection
+     */
     abstract public void exec(InitialContext ctx);
 }
