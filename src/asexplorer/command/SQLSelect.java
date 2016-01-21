52646d56b70426960612c1daefc07bd454984df0
Ferruccio Vitale <unixo@devzero.it>
2016-01-21 13:01:23 +0100

    - output formatted as table - output formatted as CSV

--- src/asexplorer/command/SQLSelect.java
+++ src/asexplorer/command/SQLSelect.java
@@ -16,6 +16,7 @@ import java.util.logging.Logger;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import org.nocrala.tools.texttablefmt.Table;
 
 /**
  *
@@ -27,6 +28,8 @@ public class SQLSelect extends CommandBase
     protected Integer colSize = 255;
     protected String sql = null;
     protected String dsName = null;
+    protected boolean isCSV = false;
+    protected String csvChar = ";";
 
     @Override
     public String getCommandName()
@@ -39,16 +42,22 @@ public class SQLSelect extends CommandBase
     {
         return "SELECT instruction for SQL datasource";
     }
+    
+    @Override
+    public String getHelp() {
+        return "--datasource str --sql str [--limit num] [--colsize num] [--csv str]";
+    }
 
     @Override
     public ArrayList<LongOpt> getParameters()
     {
         ArrayList<LongOpt> params = new ArrayList<LongOpt>();
 
-        params.add(new LongOpt("datasource", LongOpt.REQUIRED_ARGUMENT, null, 100));
+        params.add(new LongOpt("datasource", LongOpt.REQUIRED_ARGUMENT, null, 'd'));
         params.add(new LongOpt("limit", LongOpt.REQUIRED_ARGUMENT, null, 101));
         params.add(new LongOpt("sql", LongOpt.REQUIRED_ARGUMENT, null, 102));
         params.add(new LongOpt("colsize", LongOpt.REQUIRED_ARGUMENT, null, 103));
+        params.add(new LongOpt("csv", LongOpt.REQUIRED_ARGUMENT, null, 1004));
 
         return params;
     }
@@ -70,6 +79,12 @@ public class SQLSelect extends CommandBase
         } else if (param.equalsIgnoreCase("colsize")) {
             this.colSize = Integer.parseInt(value);
             retValue = true;
+        } else if (param.equalsIgnoreCase("csv")) {
+            this.isCSV = true;
+            retValue = true;
+            if (value != null) {
+                this.csvChar = value;
+            }
         }
 
         return retValue;
@@ -81,40 +96,50 @@ public class SQLSelect extends CommandBase
         if (this.sql == null || this.dsName == null) {
             asexplorer.ASExplorer.logger.error("A datasource/sql needs to be specified.");
         } else {
-            Statement stmt = null;
-            ResultSet rs = null;
-            DataSource ds = null;
-            Connection conn = null;
-
             try {
-                ds = (DataSource) ctx.lookup(this.dsName);
-                conn = ds.getConnection();
-                stmt = conn.createStatement();
+                DataSource ds = (DataSource) ctx.lookup(this.dsName);
+                Connection conn = ds.getConnection();
+                Statement stmt = conn.createStatement();
 
                 // limit result set size, if user specified an upper limit
-                if (this.limit != -1)
+                if (this.limit != -1) {
                     stmt.setMaxRows(this.limit);
+                }
 
-                rs = stmt.executeQuery(this.sql);
+                ResultSet rs = stmt.executeQuery(this.sql);
 
                 // Print all columns
                 ResultSetMetaData rsMetaData = rs.getMetaData();
                 int cols = rsMetaData.getColumnCount();
+                Table t = new Table(cols);
                 for (int i=1; i<=cols; i++) {
-                    System.out.format("| %s ", rsMetaData.getColumnName(i));
+                    if (this.isCSV) {
+                        System.out.format("%s%s", rsMetaData.getColumnName(i), this.csvChar);
+                    } else {
+                        t.addCell(rsMetaData.getColumnName(i));
+                    }
                 }
-                System.out.println();
-
+                
                 // Print all returned records
                 while(rs.next()) {
                     for (int i=1; i<=cols; i++) {
                         String value = rs.getString(i);
-                        if (value == null)
+                        if (value == null) {
                             value = "NULL";
+                        }
 
-                        System.out.format("| %."+this.colSize +"s ",value);
+                        if (this.isCSV) {
+                            System.out.format("%."+this.colSize +"s%s",value, this.csvChar);
+                        } else {
+                            t.addCell(value);
+                        }
                     }
-                    System.out.println();
+                    if (this.isCSV) {
+                        System.out.println();
+                    }
+                }
+                if (!this.isCSV) {
+                    System.out.println(t.render());
                 }
 
                 // Finally close the connection
